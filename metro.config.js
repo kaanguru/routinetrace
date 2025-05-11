7b1c1fc58b535d0b35c7db0cd0c660bd3c31c844
kaanguru <kaanguru@mail.ru>
2025-05-11 07:12:07 +0300

    Configure Sentry for error tracking
    Add @sentry/react-native/expo plugin configuration to app.json with organization and project details. Update metro.config.js to use Sentry's config directly for proper error tracking integration.

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,7 @@
-const { getDefaultConfig } = require('expo/metro-config');
+// const { getDefaultConfig } = require("expo/metro-config");
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-// First get the default Expo config
-const expoConfig = getDefaultConfig(__dirname);
-
-// Then merge with Sentry config
-const config = getSentryExpoConfig(__dirname, expoConfig);
+// const config = getDefaultConfig(__dirname);
+const config = getSentryExpoConfig(__dirname);
 
 module.exports = config;
